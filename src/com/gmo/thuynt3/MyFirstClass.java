4cfd217e11e67445528c9bf63247ba4b66fa0c4d
ThuyNT <thuynt707@gmail.com>
2019-08-15 10:52:21 +0700

    Created my first class

--- src/com/gmo/thuynt3/MyFirstClass.java
+++ src/com/gmo/thuynt3/MyFirstClass.java
@@ -3,12 +3,25 @@ package com.gmo.thuynt3;
 public class MyFirstClass {
 
 	public static void main(String[] args) {
-		//Cast value
+		// Cast value
 		int i = 50;
 		short j = (short) i;
 		double d = i;
 		System.out.println("Short value is: " + j);
 		System.out.println("Double value is: " + d);
+
+		myStaticMethod();
+		MyFirstClass oj = new MyFirstClass();
+		oj.myPublicMethod();
+
+	}
+
+	static void myStaticMethod() {
+		System.out.println("Static method can be called without creating object");
+	}
+
+	public void myPublicMethod() {
+		System.out.println("Public method must  be called by creating object");
 	}
 
 }
